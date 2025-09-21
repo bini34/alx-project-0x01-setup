45de9f5321b3b0e6c7b9b9a0d6f8e0ba580935e0
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-09-21 21:21:04 +0300

    Refactor UserModalProps to use UserProps and enhance Home and Posts pages with header and button components

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -24,7 +24,7 @@ export interface UserData {
 
 export interface UserModalProps {
   onClose: () => void;
-  onSubmit: (user: UserData) => void;
+  onSubmit: (post: UserProps) => void;
 }
 export interface PostProps {
   userId: number;
